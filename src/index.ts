56e058afdc25a5f9b09084731486f455800f417e
Magomed <scrythepvp@gmail.com>
2022-05-20 19:42:22 +0200

    feat: basic structure for game

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('hello');
+import Game from './game';
+
+const WIDTH = 960;
+const HEIGHT = 720;
+const FPS = 60;
+const MS_PER_UPDATE = 1000 / FPS;
+const canvas = document.querySelector('canvas')!;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+const ctx = canvas.getContext('2d')!;
+
+const game = new Game(ctx);
+
+let previous = performance.now();
+let lag = 0;
+
+function gameLoop(current: number) {
+  const timeDiffrence = current - previous;
+  lag += timeDiffrence;
+  while (lag >= MS_PER_UPDATE) {
+    game.update();
+    lag -= MS_PER_UPDATE;
+  }
+  game.draw();
+  previous = current;
+}
+
+requestAnimationFrame(gameLoop);
