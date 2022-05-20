436940b799b3e11ee005fba981fab7c40c0ebd68
Magomed <scrythepvp@gmail.com>
2022-05-20 19:31:56 +0200

    a

---
+++ src/game.ts
@@ -0,0 +1,10 @@
+class Game {
+  private ctx: CanvasRenderingContext2D;
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx;
+  }
+  update() {}
+  draw() {}
+}
+
+export default Game;
